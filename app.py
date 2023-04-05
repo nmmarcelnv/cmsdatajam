5476e15e5019d06d82c4e3ff2a78a252927251f0
Marcel Nguemaha <nmmarcelnv@gmail.com>
2023-04-05 13:52:14 -0400

    adding data files

--- app.py
+++ app.py
@@ -1,42 +1,89 @@
-from dash import Dash, callback, html, dcc
-import dash_bootstrap_components as dbc
-import numpy as np
+import plotly.figure_factory as ff
 import pandas as pd
-import matplotlib as mpl
-import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
-from whitenoise import WhiteNoise   #for serving static files on Heroku
+import numpy as np
+import dash
+import dash_core_components as dcc
+from dash import html
+from dash.dependencies import Input, Output
 
-# Instantiate dash app
-app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+dtypes = {
+    'StateFIPS':str,
+    'CountyFIPS_3':str,
+    'CountyName':str,
+    'StateName':str,
+    'CountyFIPS':str,
+    'StateAbbr':str,
+    'STATE_COUNTY':str
+}
+ckd = pd.read_excel('../data/Prevalence_of_CKD_by_US_State_and_County_by_County_2019.xlsx', sheet_name=1)
+fips = pd.read_csv(
+    'https://raw.githubusercontent.com/ChuckConnell/articles/master/fips2county.tsv',
+    sep='\t',
+    usecols=['CountyName','StateName','CountyFIPS','StateAbbr'],
+    dtype=dtypes
+)
 
-# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
-server = app.server 
+fips.columns=['county','state','fips', 'state3']
+fips = fips[['state','county','fips', 'state3']]
+fips['county'] = fips['county'].str.upper()
+fips['state'] = fips['state'].str.upper()
 
-# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
-# Define Dash layout
-def create_dash_layout(app):
+ckd.columns=['ckd_value','county','state']
+ckd['county'] = ckd['county'].str.upper()
+ckd['state'] = ckd['state'].str.upper()
+ckd = ckd[['state','county','ckd_value']]
 
-    # Set browser tab title
-    app.title = "Your app title" 
-    
-    # Header
-    header = html.Div([html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()])
-    
-    # Body 
-    body = html.Div([dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """), html.Br(), html.Img(src='charlie.png')])
+df = pd.merge(ckd, fips,on=['state','county'])
 
-    # Footer
-    footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built with ![Image](heart.png) in Python using [Dash](https://plotly.com/dash/)""")])
-    
-    # Assemble dash layout 
-    app.layout = html.Div([header, body, footer])
+colorscale = ["#f7fbff","#ebf3fb","#deebf7","#d2e3f3","#c6dbef","#b3d2e9","#9ecae1",
+              "#85bcdb","#6baed6","#57a0ce","#4292c6","#3082be","#2171b5","#1361a9",
+              "#08519c","#0b4083","#08306b"]
+colorscale = ["#f7fbff", "#d4e9f7", "#afd6ef", "#8fbfe8", "#7197c5", "#58719f", "#3d4f7f", "#2a2a5a", "#151531"]
+colorscale = [
+    'rgb(193, 193, 193)',
+    'rgb(239,239,239)',
+    'rgb(195, 196, 222)',
+    'rgb(144,148,194)',
+    'rgb(101,104,168)',
+    'rgb(65, 53, 132)',
+    'rgb(63.0, 188.0, 115.0)',
+]
+endpts = list(np.linspace(1, 100, len(colorscale) - 1))
+endpts = [0, 10, 20, 30, 40, 100]
+fips_values = df['fips'].tolist()
+ckd_values = df['ckd_value'].tolist()
 
-    return app
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='state-dropdown',
+        options=[{'label': state, 'value': state} for state in df['state'].unique()],
+        value='Alabama'
+    ),
+    dcc.Graph(id='ckd-map')
+])
 
-# Construct the dash layout
-create_dash_layout(app)
 
-# Run flask app
-if __name__ == "__main__": app.run_server(debug=False, host='0.0.0.0', port=8050)
+@app.callback(
+    Output('ckd-map', 'figure'),
+    Input('state-dropdown', 'value')
+)
+def update_map(selected_state):
+    filtered_df = df[df['state'] == selected_state]
+    fips_values = filtered_df['fips'].tolist()
+    ckd_values = filtered_df['ckd_value'].tolist()
+    fig = ff.create_choropleth(
+        fips=fips_values, values=ckd_values,
+        binning_endpoints=endpts,
+        colorscale=colorscale,
+        scope=[selected_state],
+        show_state_data=True,
+        show_hover=True, centroid_marker={'opacity': 0},
+        asp=2.9, title='CKD Prevalence',
+        legend_title='% CKD'
+    )
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
