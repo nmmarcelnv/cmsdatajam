c08fd3ee6d422c7a9ef1e3a6a4dd79cc72ac80f0
Marcel Nguemaha <nmmarcelnv@gmail.com>
2023-04-07 13:42:30 -0400

    adding food access data with poverty rate by county

--- utils/helpers.py
+++ utils/helpers.py
@@ -41,3 +41,43 @@ def get_ckd_by_counties()
     
     return df
 
+
+def get_povertyrate_by_county():
+    
+    data_atlas_link = 'https://www.ers.usda.gov/webdocs/DataFiles/80591/FoodAccessResearchAtlasData2019.xlsx?v=9165.3'
+    usecols = ['State','County','LILATracts_Vehicle', 'HUNVFlag', 'LowIncomeTracts', 'PovertyRate', 'MedianFamilyIncome']
+    dfatlas = pd.read_excel(data_atlas_link,sheet_name='Food Access Research Atlas', usecols=usecols)
+
+    dfatlas['State'] = dfatlas['State'].str.upper()
+    dfatlas['County'] = dfatlas['County'].apply(lambda x: x.upper().replace(' COUNTY','').strip())
+
+    dtypes = {
+        'StateFIPS':str, 
+        'CountyFIPS_3':str, 
+        'CountyName':str, 
+        'StateName':str, 
+        'CountyFIPS':str,
+        'StateAbbr':str, 
+        'STATE_COUNTY':str
+    }
+
+    fips = pd.read_csv(
+        'https://raw.githubusercontent.com/ChuckConnell/articles/master/fips2county.tsv',
+        sep='\t', 
+        usecols=['CountyName','StateName','CountyFIPS','StateAbbr'],              
+        dtype=dtypes
+    )
+
+    fips.columns=['County','State','FIPS', 'State3']
+    fips = fips[['State','County','FIPS', 'State3']]
+    fips['State'] = fips['State'].str.upper()
+    fips['County'] = fips['County'].str.upper()
+
+    dff = pd.merge(
+        dfatlas,
+        fips.drop_duplicates(),
+        on=['State', 'County'],
+    )
+    #save as dff.to_parquet('../data/FoodAccessResearchAtlasData2019.parquet')
+    
+    return dff
