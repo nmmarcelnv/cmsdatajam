00600bb7d4e89230e8fdb64f73ca2d2859e3e7d2
Marcel Nguemaha <nmmarcelnv@gmail.com>
2023-04-08 11:59:59 -0400

    added intro to app

--- app-Copy1.py
+++ app-Copy1.py
@@ -10,7 +10,7 @@ from whitenoise import WhiteNoise   #for serving static files on Heroku
 
 #get the data from public repos
 df = pd.read_parquet('https://github.com/nmmarcelnv/cmsdatajam/blob/main/data/ckd_by_county.parquet?raw=true')
-
+dff = pd.read_parquet('https://github.com/nmmarcelnv/cmsdatajam/blob/main/data/FoodAccessResearchAtlasData2019.parquet?raw=true')
 # Instantiate dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
@@ -29,52 +29,99 @@ fig = px.choropleth(
     color_continuous_scale='YlOrRd',
     range_color=(cmin, cmax),
     hover_data = {'state':True, 'county':True},
-    labels={'cases':'CKD Prevalence (%)'}
+    labels={'cases':'CKD Prevalence (%)'},
+    title='CKD Prevalence by US Counties',
+)
+fig2 = px.choropleth(
+    dff, 
+    geojson="https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json",
+    locations="FIPS", 
+    color='PovertyRate',
+    scope='usa',
+    color_continuous_scale='YlOrRd',
+    range_color=(cmin, cmax),
+    hover_data = {'State':True, 'County':True},
+    labels={'PovertyRate':'Poverty Rate'},
+    title='Poverty Rate by US Counties',
 )
 
 # Create layout with input components and the choropleth map
-app.layout = html.Div([
-    dbc.Row([
-        dbc.Col(
-            dcc.Input(
-                id='cmin-input',
-                type='number',
-                value=cmin,
-                min=0,
-                max=100,
-                step=1,
-                placeholder='Enter a min value'
-            ), 
-            width={'size': 2, 'offset': 1}
+# Set the layout of the app
+app.layout = html.Div(
+    style={'backgroundColor': '#f2f2f2', 'border': '5px solid #555'},
+    children=[
+        # Header section
+        html.Div(
+            className="header",
+            children=[
+                html.H1("2023 Health Equity Datajam: A Practical Guide Reduce Chronic Kidney Disease (CKD)"),
+                html.H5([
+                    "Contributors: ",
+                    html.Span("Marcel Nguemaha", style={"color": "blue", "font-weight": "bold"}),
+                    " and ",
+                    html.Span("Priyanka Garde", style={"color": "blue", "font-weight": "bold"}),
+                    " (National Government Services)"
+                ]),
+                html.Hr(),
+                html.P(
+            "According to Center of Disease Control (CDC) reports, more than 15% of US adults are estimated to have CKD and 9 in 10 adults with CKD are unaware of their condition. Also, African American and other minority population, mostly living in under-served communities are more that 4 times as likely as Whites to develop Kidney Failure."
+                ),
+            ]
         ),
-        dbc.Col(
-            dcc.Input(
-                id='cmax-input',
-                type='number',
-                value=cmax,
-                min=0,
-                max=100,
-                step=1,
-                placeholder='Enter a max value'
-            ), 
-            width={'size': 2, 'offset': 1}
-        ),
-    ], align='center', justify='center', className='mt-3 mb-3'),
-    dbc.Row([
-        dbc.Col(
-            dcc.Graph(id='ckd-map', figure=fig),
-            width={'size': 10, 'offset': 1}
-        )
-    ])
-])
 
-# Create a callback to update the range_color of the choropleth map based on the input values
+        # Main content section
+        html.Div(
+            className="main-content",
+            children=[
+                # Input panel section
+                html.Div(
+                    className="input-panel",
+                    children=[
+                        html.Label('Select CKD Prevalence Range:'),
+                        dcc.RangeSlider(
+                            id='c-range-slider',
+                            min=0,
+                            max=100,
+                            step=1,
+                            value=[cmin, cmax],
+                            marks={x: str(x) for x in [0,20,40,60,80,10]}
+                        ),
+                    ],
+                    style={'width': '48%', 'display': 'inline-block'}
+                ),
+
+                # Map section
+                html.Div(
+                    className="map-section",
+                    children=[
+                        dcc.Graph(
+                            id='ckd-map',
+                            figure=fig,
+                            style={'width': '48%', 'display': 'inline-block'}
+                        ),
+                        dcc.Graph(
+                            id='poverty-map',
+                            figure=fig2,
+                            style={'width': '48%', 'display': 'inline-block', 'float': 'right'}
+                        )
+                    ]
+                ),
+            ]
+        ),
+    ],
+    className="container"
+)
+
+
+# Create a callback to update the range_color of the choropleth map based on the slider value
 @app.callback(
     Output('ckd-map', 'figure'),
-    Input('cmin-input', 'value'),
-    Input('cmax-input', 'value')
+    Input('c-range-slider', 'value')
 )
-def update_map(cmin, cmax):
+def update_map(range_values):
+    cmin = range_values[0]
+    cmax = range_values[1]
+    
     fig = px.choropleth(
         df, 
         geojson="https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json",
@@ -84,8 +131,10 @@ def update_map(cmin, cmax):
         color_continuous_scale='YlOrRd',
         range_color=(cmin, cmax),
         hover_data = {'state':True, 'county':True},
-        labels={'cases':'CKD Prevalence (%)'}
+        labels={'cases':'CKD Prevalence (%)'},
+        title='CKD Prevalence by US Counties',
     )
+    
     return fig
 
 # Run flask app
